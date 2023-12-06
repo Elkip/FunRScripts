9b0fe388c42b4a3dd005c05ca9fe5e4ac0b26ddb
elkip <mitchiscool51@gmail.com>
2023-12-06 14:39:29 -0500

    added interactive map with slider for year

--- aliens.R
+++ aliens.R
@@ -7,11 +7,12 @@ library(tidycensus) # For FIPS codes
 aliens_raw <- read.csv("~/Datasets/aliens_scrubbed.csv")
 
 ## Clean Data
-which(!grepl('^-?(0|[1-9][0-9]*)(\\.[0-9]+)?$', aliens_raw[["duration..seconds."]]))
+which(!grepl('^-?(0|[1-9][0-9]*)(\\.[0-9]+)?$', 
+             aliens_raw[["duration..seconds."]]))
 # aliens_raw[c(27823, 35693, 41202, 58592),]
 # Fix seconds row in the above columns
 aliens_raw[["duration..seconds."]] <- trimws(gsub("`", "", 
-                                                  aliens_raw[["duration..seconds."]]))
+                                        aliens_raw[["duration..seconds."]]))
 # Remove extra character from latitude
 # aliens_raw[43783,]
 aliens_raw[["latitude"]] <- trimws(gsub("[^0-9.-]", "", 
@@ -30,19 +31,20 @@ aliens <- aliens_raw |> mutate(datetime = as_datetime(datetime,
 #glimpse(aliens)
 #esquisse::esquisser(aliens)
 
-## Map by state
-aliens_us <- aliens |> filter(country == 'us') 
+# Just the 'merician aliens
+aliens_us <- aliens |> filter(country == 'us')
 
+## Map by state
 aliens_by_state <- aliens_us |>
     group_by(state) |> 
     count(state)
 
 plot_usmap(data = aliens_by_state, values = "n") +
-    scale_fill_continuous(name = "Alien Sightings Between 1998-2015") +
+    scale_fill_continuous(name = "UFO Sightings Between 1910-2015") +
     theme(legend.position = "right")
 
 ## Map by county
-## Return a dictionary of lat/long coordinates and coresponding county
+## Return a dictionary of lat/long coordinates and corresponding county
 coords_to_county <- function(pointsDF, states) {
     # Transform coordinates to account for Alaska/Hawaii
     pts <- usmap_transform(pointsDF, input_names = c("longitude", "latitude"))
@@ -66,10 +68,8 @@ coords_to_county <- function(pointsDF, states) {
         mutate(county = str_remove_all(tolower(county), "\\b (?:county|parish)$"))
 }
 
-# pointsDF <- aliens_us[,c("longitude","latitude")]
-
 # Get county list and create geometry map
-state_list <- usmap::us_map('counties') %>%
+state_list <- usmap::us_map('counties') |>
     rename(state = abbr)
 state_list <- do.call('rbind', split(state_list, 
                                      paste(state_list$state, 
@@ -77,7 +77,8 @@ state_list <- do.call('rbind', split(state_list,
                                            state_list$piece)) |>
                           lapply(function(x) st_sf(county = x$county[1], 
                                                    state = x$state[1],
-                                                   st_sfc(st_polygon(list(cbind(x$x, x$y)))), 
+                                                   st_sfc(st_polygon(
+                                                       list(cbind(x$x, x$y)))),
                                                    crs = usmap::usmap_crs())))
 
 # Transform spatial data to Mercator coordinate system and get county
@@ -86,7 +87,14 @@ county_dict <- coords_to_county(aliens_us[,c("longitude","latitude")], state_lis
 aliens_w_county <- merge(aliens_us, county_dict, by = c("longitude","latitude")) 
 
 aliens_w_county <- aliens_w_county |> 
-    mutate(full_county = paste(tolower(state.name[match(toupper(aliens_w_county$state), state.abb)]),aliens_w_county$county, sep=","))
+    mutate(full_county = 
+        paste(
+            tolower(state.name[
+                match(
+                    toupper(aliens_w_county$state), state.abb)]
+            ),
+            aliens_w_county$county, sep=",")
+        )
 
 # Get count by county
 aliens_by_county <- aliens_w_county |>
@@ -97,8 +105,10 @@ aliens_by_county <- aliens_w_county |>
 # Get FIPS from tidycensus
 data("fips_codes")
 fips <- fips_codes |>
-    mutate(fips = as.numeric(paste(state_code, county_code, sep = "")),
-           full_county = str_remove_all(tolower(paste(state_name, county, sep = ",")), 
+    mutate(fips = paste(state_code, county_code, sep = ""),
+           full_county = str_remove_all(
+               tolower(
+                   paste(state_name, county, sep = ",")), 
                                         "\\b (?:county|parish)$")) |>
     select(c(fips, full_county))
 
@@ -108,6 +118,46 @@ aliens_by_county <- aliens_by_county |>
     left_join(fips, by = "full_county")
 
 # Create map
-plot_usmap(regions = "counties", data = aliens_by_county, values = "n") +
-    scale_fill_continuous(name = "Alien Sightings Between 1998-2015") +
+plot_usmap(regions = "counties", data = aliens_by_county, 
+           values = "n") +
+    scale_fill_continuous(name = "UFO Sightings Between 1998-2015") +
     theme(legend.position = "right")
+
+## Trying something interactive with a slider for date
+library(plotly)
+library(rjson)
+
+# Get count by year and county
+aliens_by_yearCounty <- aliens_w_county |>
+    mutate(year = as.numeric(format(datetime, '%Y'))) |>
+    group_by(year, full_county) |> 
+    count(year, full_county) |>
+    filter(!grepl('NA', full_county)) |>
+    left_join(fips, by = "full_county")
+
+
+url <- 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
+counties <- rjson::fromJSON(file=url)
+g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showlakes = TRUE,
+    lakecolor = toRGB('white')
+)
+plot_ly() |>
+    add_trace(
+        type = "choropleth",
+        geojson = counties,
+        locations = aliens_by_yearCounty$fips,
+        z = aliens_by_yearCounty$n,
+        zmin = 0,
+        zmax = 150,
+        frame = aliens_by_yearCounty$year,
+        autocolorscale = FALSE,
+        colorscale = "Greens",
+        reversescale = TRUE
+    ) |>
+    colorbar(title = "Number of UFO Sightings", 
+             tickvals = c(10, 25, 50, 100, 150)
+        ) |>
+    layout(title = 'UFO Sightings 1998-2015', geo = g)
